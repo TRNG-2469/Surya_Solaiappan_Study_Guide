062278ce5f8c2f399228520d69643db81ff404ef
snsorl <145521182+snsorl@users.noreply.github.com>
2026-08-20 14:48:44 -0500

    updated yesterday exercise 1

--- Week5-Spring/exercise/3-wednesday/Employee.java
+++ Week5-Spring/exercise/3-wednesday/Employee.java
@@ -5,10 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
     private int id;

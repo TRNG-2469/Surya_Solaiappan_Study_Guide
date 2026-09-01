4534953835e0b910ec7a0894451e29685ccd7e24
snsorl <145521182+snsorl@users.noreply.github.com>
2026-09-01 08:18:24 -0500

    Monday Exercises

---
+++ Week7-TypeScript-Angular/exercise/1-monday/app/student-card/student-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-card',
+  imports: [],
+  templateUrl: './student-card.component.html',
+  styleUrl: './student-card.component.css',
+})
+export class StudentCard {}
---
+++ Week7-TypeScript-Angular/exercise/1-monday/app/student-list/student-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-list',
+  imports: [],
+  templateUrl: './student-list.component.html',
+  styleUrl: './student-list.component.css',
+})
+export class StudentList {}
